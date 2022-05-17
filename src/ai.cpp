e0e2e057eb9e20b26f7494e1057ba4b2aa7c14ab
trineutron <programgoo@gmail.com>
2022-05-17 16:22:44 +0900

    動かせる場所の処理を実装

--- src/ai.cpp
+++ src/ai.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <random>
 #include <sstream>
+#include <vector>
 
 constexpr int8_t board_startpos[9][9]{
     {-2, -3, -4, -5, -8, -5, -4, -3, -2},
@@ -14,8 +17,10 @@ constexpr int8_t board_startpos[9][9]{
     {2, 3, 4, 5, 8, 5, 4, 3, 2},
 };
 
+std::mt19937_64 rng;
+
 void usi_init() {
-    std::cout << "id name test" << std::endl;
+    std::cout << "id name kikishogi" << std::endl;
     std::cout << "id author trineutron" << std::endl;
     std::cout << "usiok" << std::endl;
 }
@@ -43,6 +48,32 @@ int get_type(char c) {
         default:
             return 0;
     }
+}
+
+const std::vector<std::pair<int, int>> type_dir[9]{
+    {},
+    {{-1, 0}},
+    {{-1, 0}},
+    {{-2, -1}, {-2, 1}},
+    {{-1, -1}, {-1, 0}, {-1, 1}, {1, -1}, {1, 1}},
+    {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}},
+    {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}},
+    {{-1, 0}, {0, -1}, {0, 1}, {1, 0}},
+    {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
+
+constexpr bool type_run[9]{false, false, true, false, false,
+                           false, true,  true, false};
+
+void copy_board(const int8_t board_in[9][9], int8_t board_out[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            board_out[i][j] = board_in[i][j];
+        }
+    }
+}
+
+bool in_board(const int x, const int y) {
+    return 0 <= x and x < 9 and 0 <= y and y < 9;
 }
 
 void move(int8_t board_current[9][9], int turn, const std::string &word) {
@@ -78,16 +109,10 @@ void get_board(const std::string &s, int8_t board_out[9][9], int &turn) {
         ss >> word;
         if (not ss) break;
         if (word == "position") {
-            continue;
         } else if (word == "startpos") {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    board_out[i][j] = board_startpos[i][j];
-                }
-            }
+            copy_board(board_startpos, board_out);
         } else if (word == "sfen") {
             std::cerr << "Warning: sfen" << std::endl;
-            continue;
         } else if (word == "moves") {
             is_move_mode = true;
             turn = 1;
@@ -109,26 +134,59 @@ void print_board(const int8_t board_print[9][9]) {
     }
 }
 
-std::string bestmove(int8_t board[9][9], int turn) {
-    std::string res;
+std::string bestmove(const int8_t board[9][9], int turn) {
+    std::vector<std::string> res;
     for (int i = 0; i < 9; i++) {
-        if (not res.empty()) break;
         for (int j = 0; j < 9; j++) {
             const int type = turn * board[i][j];
             if (type <= 0) continue;
-            if (type == 3 or type == 6) continue;
-            const int i_next = i - turn;
-            if (i_next < 0 or 9 <= i_next) continue;
-            if (turn * board[i_next][j] > 0) continue;
-            res += '9' - j;
-            res += i + 'a';
-            res += '9' - j;
-            res += i_next + 'a';
-            if (i_next == 0 or i_next == 8) res += '+';
-            break;
+            int type_move[2]{type, 0};
+            if (9 <= type and type <= 13) {
+                type_move[0] = 5;
+            } else if (type >= 14) {
+                type_move[0] = type - 8;
+                type_move[1] = 8;
+            }
+            for (int k = 0; k < 2; k++) {
+                int run_limit = type_run[type_move[k]] ? 8 : 1;
+                for (auto &&[dx, dy] : type_dir[type_move[k]]) {
+                    for (int run_idx = 1; run_idx <= run_limit; run_idx++) {
+                        const int i_next = i + run_idx * turn * dx;
+                        const int j_next = j + run_idx * dy;
+                        if (not in_board(i_next, j_next)) break;
+                        if (turn * board[i_next][j_next] > 0) break;
+                        std::string s;
+                        s += '9' - j;
+                        s += i + 'a';
+                        s += '9' - j_next;
+                        s += i_next + 'a';
+                        bool can_promote, must_promote;
+                        if (turn == 1) {
+                            can_promote = i < 3 or i_next < 3;
+                            must_promote = (i_next < 2 and type == 3) or
+                                           (i_next == 0 and type <= 2);
+                        } else {
+                            can_promote = i >= 6 or i_next >= 6;
+                            must_promote = (i_next >= 7 and type == 3) or
+                                           (i_next == 8 and type <= 2);
+                        }
+                        if (type == 5 or type >= 8) can_promote = false;
+                        if (not must_promote) res.push_back(s);
+                        if (can_promote) {
+                            s += '+';
+                            res.push_back(s);
+                        }
+                        if (board[i_next][j_next]) break;
+                    }
+                }
+            }
         }
     }
-    return res;
+    if (res.empty()) {
+        return "resign";
+    }
+    std::shuffle(res.begin(), res.end(), rng);
+    return res.front();
 }
 
 int main() {
